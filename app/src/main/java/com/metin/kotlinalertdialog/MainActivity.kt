0f0fab5ad58d4b8fb094341a619ee0267f51ea5a
metinsut <metindir@gmail.com>
2020-12-06 22:14:50 +0300

    first commit

---
+++ app/src/main/java/com/metin/kotlinalertdialog/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.metin.kotlinalertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext, "Welcome", Toast.LENGTH_SHORT).show()
+    }
+
+    fun save(view : View) {
+        val alert = AlertDialog.Builder(this@MainActivity);
+        alert.setTitle("Save");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes"){
+            dialog, which ->
+            Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No"){
+            dialog, which ->
+            Toast.makeText(applicationContext,"Not Saved",Toast.LENGTH_LONG).show()
+        }
+        alert.show();
+    }
+}
